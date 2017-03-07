1d6679bca49f671390d90d6f06cd4d14635b6df7
jnrubiano <jnrubiano@gmail.com>
2017-03-07 12:44:05 -0500

    Version 2: Graficador extrayendo datos de la DB

--- src/Graficador.java
+++ src/Graficador.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import org.jfree.chart.*;
 import org.jfree.chart.plot.PlotOrientation;
@@ -19,97 +20,116 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 public class Graficador {
 
+	private static String numeroDatos = "";
+	private static String dato = "";
 
-	public static void main(String[] args) {		
-		
-		XYSeries datos1 = new XYSeries("Señal cardiaca");
-	
-		datos1.add(1.0,3.0);
-		datos1.add(1.2,4.5);
-		datos1.add(1.4,1.2);
-		datos1.add(1.6,3.0);
-		datos1.add(2.0,3.0);
-		
-		datos1.add(2.0,3.0);
-		datos1.add(2.2,4.5);
-		datos1.add(2.4,1.2);
-		datos1.add(2.6,3.0);
-		datos1.add(3.1,3.0);
-		
-		datos1.add(3.0,3.0);
-		datos1.add(3.2,4.5);
-		datos1.add(3.4,1.2);
-		datos1.add(3.6,3.0);
-		datos1.add(4.1,3.0);
-		
-		datos1.add(4.0,3.0);
-		datos1.add(4.2,4.5);
-		datos1.add(4.4,1.2);
-		datos1.add(4.6,3.0);
-		datos1.add(5.1,3.0);
-		
-		datos1.add(5.0,3.0);
-		datos1.add(5.2,4.5);
-		datos1.add(5.4,1.2);
-		datos1.add(5.6,3.0);
-		datos1.add(6.1,3.0);
-		
-		datos1.add(6.0,3.0);
-		datos1.add(6.2,4.5);
-		datos1.add(6.4,1.2);
-		datos1.add(6.6,3.0);
-		datos1.add(7.1,3.0);
-		
-		datos1.add(7.0,3.0);
-		datos1.add(7.2,4.5);
-		datos1.add(7.4,1.2);
-		datos1.add(7.6,3.0);
-		datos1.add(8.1,3.0);
-		
-		datos1.add(8.0,3.0);
-		datos1.add(8.2,4.5);
-		datos1.add(8.4,1.2);
-		datos1.add(8.6,3.0);
-		datos1.add(9.1,3.0);
-		
-		datos1.add(9.0,3.0);
-		datos1.add(9.2,4.5);
-		datos1.add(9.4,1.2);
-		datos1.add(9.6,3.0);
-		datos1.add(10.1,3.0);
-		
-		datos1.add(5.0,3.0);
-		datos1.add(5.2,4.5);
-		datos1.add(5.4,1.2);
-		datos1.add(5.6,3.0);
-		datos1.add(6.1,3.0);
+	private static ArrayList<String> dato2 = new ArrayList<String>();
+	private static ArrayList<String> dato3 = new ArrayList<String>();
+	private static ArrayList<String> dato4 = new ArrayList<String>();
+	private static ArrayList<String> dato5 = new ArrayList<String>();
 
-		
-		
+	private static XYSeries datos1 = new XYSeries("Señal cardiaca");
+
+	public static String separarDato(String n) {
+		char[] temp = n.toCharArray();
+		String resultado = "";
+		int cont = 0;
+		for (int i = 0; i < temp.length; i++) {
+			if (temp[i] != ',') {
+				resultado += temp[i];
+			}
+			if ((temp[i] == ',' || temp[i] == temp.length + 1) && cont == 0) {
+				dato2.add(resultado);
+				resultado = "";
+				cont++;
+			} else if ((temp[i] == ',' || temp[i] == temp.length + 1) && cont == 1) {
+				dato3.add(resultado);
+				resultado = "";
+				cont++;
+			} else if ((temp[i] == ',' || temp[i] == temp.length + 1) && cont == 2) {
+				dato4.add(resultado);
+				resultado = "";
+				cont++;
+			}
+		}
+		dato5.add(resultado);
+		return resultado;
+	}
+
+	public static void leerArchivo() throws IOException {
+		int contadorLineas = 0;
+		File archivo = new File("../../Physionet/Datos.txt");
+		FileReader fr;
+
+		try {
+			fr = new FileReader(archivo);
+			BufferedReader br = new BufferedReader(fr);
+			String linea;
+			while ((linea = br.readLine()) != null) {
+				contadorLineas++;
+				if (contadorLineas <= Integer.valueOf(numeroDatos)) {
+					separarDato(linea);
+				}
+
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void imprimir() {
+		System.out.println();
+		for (int i = 0; i < dato2.size(); i++) {
+			System.out.println(dato2.get(i));
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		numeroDatos = JOptionPane.showInputDialog(null, "Ingrese el número de datos que desea ver:", "Datos",
+				JOptionPane.QUESTION_MESSAGE);
+		dato = JOptionPane.showInputDialog(null, "Ingrese el dato que desea ver:", "Datos",
+				JOptionPane.QUESTION_MESSAGE);
+
+		ArrayList<String> datoVer = new ArrayList<String>();
+
+		if (Integer.valueOf(dato) == 1) {
+			datoVer = dato3;
+		} else if (Integer.valueOf(dato) == 2) {
+			datoVer = dato4;
+		} else if (Integer.valueOf(dato) == 3) {
+			datoVer = dato5;
+		}
+
+		leerArchivo();
+
+		for (int i = 0; i < dato2.size(); i++) {
+			datos1.add(Double.parseDouble(dato2.get(i)), Double.parseDouble(datoVer.get(i)));
+		}
+
+		imprimir();
+
 		XYSeriesCollection dataset = new XYSeriesCollection();
 		dataset.addSeries(datos1);
-		
-		JFreeChart xyLineChart = ChartFactory.createXYLineChart(
-				"Prueba Gráfica Señales", 
-				"", 
-				"", 
-				dataset, 
+
+		JFreeChart xyLineChart = ChartFactory.createXYLineChart("Señal cardiaca", "", "", dataset,
 				PlotOrientation.VERTICAL, true, true, false);
-		
+
 		XYPlot plot = xyLineChart.getXYPlot();
-		
+
 		XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
 		renderer.setSeriesPaint(0, Color.BLACK);
-		renderer.setSeriesStroke(0, new BasicStroke(2.0f));
+		renderer.setSeriesStroke(0, new BasicStroke(0.5f));
 		plot.setRenderer(renderer);
-		
+
 		ChartPanel panel = new ChartPanel(xyLineChart);
-		
+
 		JFrame ventana = new JFrame("Gráfica Señales");
 		ventana.setVisible(true);
 		ventana.setSize(800, 600);
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		ventana.add(panel);
 	}
 }
